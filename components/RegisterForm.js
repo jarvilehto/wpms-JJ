124a4acfb61ad70e9beda3bbdb45102dbd0a393f
jarvilehto <jarvilehtojuhoj@gmail.com>
2022-09-05 14:49:55 +0300

    Task B

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,48 +1,109 @@
 import React, {useContext} from 'react';
-import { TurboModuleRegistry, View} from 'react-native';
+import {Alert, View} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import {useUser} from '../hooks/ApiHooks';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {MainContext} from '../context/MainContext';
-import {
-  Button,
-  Input,
-  Text,
-} from '@rneui/base';
+import {Button, Input, Text} from '@rneui/base';
 
 const RegisterForm = () => {
-  const {isLoggedIn, setIsLoggedIn, setFormToggle, formToggle} = useContext(MainContext);
+  const {isLoggedIn, setIsLoggedIn, setFormToggle, formToggle} =
+    useContext(MainContext);
   const {
     control,
     handleSubmit,
     formState: {errors},
+    getValues,
   } = useForm({
     defaultValues: {
       username: '',
       password: '',
+      confirmPassword:'',
       email: '',
       full_name: '',
     },
     mode: 'onBlur',
   });
 
-  const onSubmit =  async (data) => {
-    console.log(data)
-    const logData = await useUser().postUser(data);
-    console.log('logData', logData);
+  //rewrite this bit!
+  const onSubmit = async (data) => {
+    console.log(data);
+    try {
+      delete data.confirmPassword;
+      const logData = await useUser().postUser(data);
+      if(logData){
+        Alert.alert('Success', 'Account has been created');
+        setFormToggle(true);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <View>
       <View style={{display: 'flex'}}>
-      <Text style={{fontSize: 20, alignSelf: 'center', fontWeight:'600'}}>Register</Text>
+        <Text style={{fontSize: 20, alignSelf: 'center', fontWeight: '600'}}>
+          Register
+        </Text>
         <Controller
           control={control}
           rules={{
-            required: true,
+            required: {value: true, message: 'Username is required!'},
             validate: async (value) => {
-              if(!await useUser().checkUsername(value)){
-                return('Username is already taken!')
+              if (!(await useUser().checkUsername(value))) {
+                return 'Username is already taken!';
+              }
+              return true;
+            },
+            minLength: {
+              value: 3,
+              message: 'Username has to be at least 3 characters!',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              placeholder="Username"
+              errorMessage={errors.username && errors.username.message}
+            />
+          )}
+          name="username"
+        />
+
+        {errors.firstName && <Text>This is required.</Text>}
+
+        <Controller
+          control={control}
+          rules={{
+            required: {value: true, message: 'Password is required!'},
+            minLength: {
+              value: 5,
+              message: 'Password has to be at least 5 characters',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              placeholder="Password"
+              autoCapitalize="none"
+            />
+          )}
+          name="password"
+        />
+
+        <Controller
+          control={control}
+          rules={{
+            required: {value: true, message: 'This field is required!'},
+            validate: (value) => {
+              const {password} = getValues();
+              if(!value == password){
+                return "Passwords dont match";
               }
               return true;
             }
@@ -52,69 +113,56 @@ const RegisterForm = () => {
               onBlur={onBlur}
               onChangeText={onChange}
               value={value}
-              placeholder='Username'
-              errorMessage={errors.username && errors.username.message}
+              placeholder="Confirm Password"
+              autoCapitalize="none"
             />
           )}
-          name="username"
+          name="confirmPassword"
         />
-      
-      {errors.firstName && <Text>This is required.</Text>}
 
-      <Controller
-        control={control}
-        rules={{
-          maxLength: 20,
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            
-            placeholder='Password'
-            autoCapitalize='none'
-          />
-        )}
-        name="password"
-        
-      />
-      
-
-<Controller
+        <Controller
           control={control}
           rules={{
-            required: true,
+            required: {value: true, message: 'Email is required!'},
+            pattern: {
+              value:
+                /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+              message: 'Not an email address!',
+            },
           }}
           render={({field: {onChange, onBlur, value}}) => (
             <Input
               onBlur={onBlur}
               onChangeText={onChange}
               value={value}
-              placeholder='email'
-              keyboardType='email-address'
-              textContentType='emailAddress'
+              placeholder="email"
+              keyboardType="email-address"
+              textContentType="emailAddress"
             />
           )}
           name="email"
         />
 
-<Controller
+        <Controller
           control={control}
           rules={{
             required: true,
+            minLength: {
+              value: 3,
+              message: 'Name has to be at least 3 characters.',
+            },
           }}
           render={({field: {onChange, onBlur, value}}) => (
             <Input
               onBlur={onBlur}
               onChangeText={onChange}
               value={value}
-              placeholder='Full name'
+              placeholder="Full name"
             />
           )}
           name="full_name"
         />
-        </View>
+      </View>
       <Button title="Register" onPress={handleSubmit(onSubmit)} />
       <Button
         title="Log In!"
